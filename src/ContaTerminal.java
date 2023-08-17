e0dbffa798cc4d3b2d854674615c960c0c54186d
Felipe Bastos <felipe.bastos.dev@gmail.com>
2023-08-17 09:43:47 -0300

    finish challenge

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número da conta: ");
+        Integer numeroDaConta = sc.nextInt();
+
+        System.out.print("Digite o número da agência: ");
+        String agencia = sc.next();
+
+        System.out.print("Digite seu nome: ");
+        sc.nextLine();
+        String nomeDoCliente = sc.nextLine();
+
+        System.out.print("Digite o saldo da conta: ");
+        double saldoDaConta = sc.nextDouble();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+                nomeDoCliente, agencia, numeroDaConta, saldoDaConta);
+
+        sc.close();
     }
 }
